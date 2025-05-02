0b53e70926417dc72614dd1460186b3dc40506de
vyk1262 <vyk1262@gmail.com>
2025-05-02 22:47:49 +0530

    text modified in home page

--- lib/widgets/home_page_widgets/hero.dart
+++ lib/widgets/home_page_widgets/hero.dart
@@ -1,7 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:skill_factorial/widgets/grid_dot_paint.dart';
 
 import 'cta_button.dart';
 
